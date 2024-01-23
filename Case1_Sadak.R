77f0764fd091a77cdf106a591567a6631ee1a516
Sadak12030 <sadak12030@gmail.com>
2024-01-23 11:08:06 -0800

    Loading excel

--- Case1_Sadak.R
+++ Case1_Sadak.R
@@ -1,1 +1,7 @@
 rm(list = ls())
+setwd("~/Desktop/MSBA DUC/6.5504/L2/Chapter 14")
+dir.exists("~/Desktop/MSBA DUC/6.5504/L2/Chapter 14")
+my_path<- '/Desktop/MSBA DUC/6.5504/L2/Chapter 14'
+library(readxl)
+buckeyecreek <- read_excel("BuckeyeCreek.xlsx")
+summary(buckeyecreek)
